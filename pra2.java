b5940fbb87dbd5d79f45f880b3f34768d6460e07
tsunokake <r.tsunokake.sys22@morijyobi.ac.jp>
2022-09-15 12:41:47 +0900

    pra2.javaに 100回連続処理を追記

--- pra2.java
+++ pra2.java
@@ -1,7 +1,7 @@
 public class pra2{
 	public static void main(String args[]){
 		String i =("Hello World");
-		for(string i = 0; i<=100; i++){
+		for(string i = 1; i<=100; i++){
 		System.out.println(i);
 		}
 	}
