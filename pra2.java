a4bf43f0412d59445099cdc02e97922ade120ca4
tsunokake <r.tsunokake.sys22@morijyobi.ac.jp>
2022-09-15 10:20:34 +0900

    pra2.javaの出力処理を 100回に変更

--- pra2.java
+++ pra2.java
@@ -1,5 +1,8 @@
 public class pra2{
 	public static void main(String args[]){
-		System.out.println("Hello World");
+		String i =("Hello World");
+		for(string i = 0; i<=100; i++){
+		System.out.println(i);
+		}
 	}
 }
